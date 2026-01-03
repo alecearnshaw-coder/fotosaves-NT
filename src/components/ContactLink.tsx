6e92b5341c065c43293979ccecb7eba1c379d7e1
Alec Earnshaw <Alec.Earnshaw@gmail.com>
2026-01-02 21:47:14 -0300

    Simplify ContactLink to use regular mailto link without JavaScript event handlers

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -1,27 +1,6 @@
 export default function ContactLink() {
-  const handleClick = (e: MouseEvent | KeyboardEvent) => {
-    e.preventDefault();
-    window.location.href =
-      'mailto:' +
-      'fotosaves.contact' +
-      '@' +
-      'gmail.com' +
-      '?subject=FotosAves%20feedback';
-  };
-
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (e.key === 'Enter') {
-      handleClick(e);
-    }
-  };
-
   return (
-    <a
-      href="#"
-      role="link"
-      onClick={handleClick}
-      onKeyDown={handleKeyDown}
-    >
+    <a href={`mailto:fotosaves.contact@gmail.com?subject=FotosAves%20feedback`}>
       Contáctame aquí - Contact me here
     </a>
   );
